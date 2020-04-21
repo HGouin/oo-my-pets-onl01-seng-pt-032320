1f67b1d7ef63703f0ccc5df1478d24f09e3484ed
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-21 19:16:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_accessor :owner, :mood
   attr_reader :name
-  def initalize(name, owner)
+  def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
