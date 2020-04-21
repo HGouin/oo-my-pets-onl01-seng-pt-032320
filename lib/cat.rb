c45b4744e32088c5c053d05cec19d77f2e05a7cb
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-21 19:13:03 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,12 @@
 class Cat
+
+@@all = []
   attr_accessor :owner, :mood
   attr_reader :name
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    
   end
 end
